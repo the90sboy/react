1cc48d42f15da695f082314bfd439152939996fa
KAFUI AYEKPLE <ayekple360@gmail.com>
2023-03-14 13:35:23 +0000

    my first react project

---
+++ Desktop/reactjs-practice/my-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <h1 style={{backgroundColor:"burlywood", fontSize:"70px", weight:"3000"}}>Codetrain is awesome, react works!!!!</h1>
+  );
+}
+
+export default App;
